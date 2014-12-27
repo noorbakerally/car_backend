7c602447df1f043d17617099650a4f27c0535e5c
Noorani Bakerally <noorani.bakerally@gmail.com>
2014-12-27 21:14:57 +0400

    method for getting specific cars added

--- api.py
+++ api.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 cars = [{'color': 'red', 'brand': 'BMW', 'plateNumber': '567hghh', 'id': 1}, {'color': 'blue', 'brand': 'Ford', 'plateNumber': '6776hhgd', 'id': 2}, {'color': 'red', 'brand': 'BMW', 'plateNumber': '6887hhgd', 'id': 3}]
 
+@app.route("/cars/<car_id>",methods=['GET'])
+def GET_specific_cars(car_id):
+	for i in range(len(cars)):
+                if (str(cars[i]['id']) == str(car_id)):
+                        return str(json.dumps(cars[i]))
+        return "id doesn't exist"
+
+
 @app.route("/cars",methods=['GET'])
 def GET_cars():
 	return str(json.dumps(cars))
