1c8c244ed96e6c28da60d2c7c1cedb6502b4e547
Noorani Bakerally <noorani.bakerally@gmail.com>
2014-12-27 20:58:14 +0400

    Update method added

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask,request
 import json
 app = Flask(__name__)
 
-cars = [{'color': 'red', 'brand': 'BMW', 'plateNumber': '567hghh', 'id': 1}, {'color': 'blue', 'brand': 'Ford', 'plateNumber': '6776hhgd', 'id': 2}, {'color': 'red', 'brand': 'BMW', 'plateNumber': '6887hhgd', 'id': 3}];
+cars = [{'color': 'red', 'brand': 'BMW', 'plateNumber': '567hghh', 'id': 1}, {'color': 'blue', 'brand': 'Ford', 'plateNumber': '6776hhgd', 'id': 2}, {'color': 'red', 'brand': 'BMW', 'plateNumber': '6887hhgd', 'id': 3}]
 
 @app.route("/cars",methods=['GET'])
 def GET_cars():
@@ -18,6 +18,27 @@ def POST_cars():
 	newCar['id'] = len(cars)
 	cars.append(newCar)
 	return str(json.dumps(newCar))
+
+@app.route("/cars/<car_id>",methods=['DELETE'])
+def DELETE_cars(car_id):
+	for i in range(len(cars)):
+		if (str(cars[i]['id']) == str(car_id)):
+			del cars[i] 
+			return "deleted"
+	return "id doesn't exist"
+	
+
+@app.route("/cars/<car_id>",methods=['PUT'])
+def UPDATE_cars(car_id):
+	args = json.loads(request.data)
+	for i in range(len(cars)):
+                if (str(cars[i]['id']) == str(car_id)):
+			cars[i]['color'] = args['color']
+        		cars[i]['brand'] = args['brand']
+        		cars[i]['plateNumber'] =  args['plateNumber']
+			return str(json.dumps(cars[i]))
+	return "id doesn't exist"	
+	
 
 if __name__ == "__main__":
 	app.debug = True;
