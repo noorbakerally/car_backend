41ed7df9349e09cb18532d3a37cd17dd38c20d49
Noorani Bakerally <noorani.bakerally@gmail.com>
2014-12-28 10:13:03 +0400

    cross origin added

--- api.py
+++ api.py
@@ -1,12 +1,11 @@
 from flask import Flask,request
 import json
-from cross_origin import crossdomain
+from flask.ext.cors import CORS
 app = Flask(__name__)
-
+CORS(app, resources='*', allow_headers='Content-Type')
 cars = [{'color': 'red', 'brand': 'BMW', 'plateNumber': '567hghh', 'id': 1}, {'color': 'blue', 'brand': 'Ford', 'plateNumber': '6776hhgd', 'id': 2}, {'color': 'red', 'brand': 'BMW', 'plateNumber': '6887hhgd', 'id': 3}]
 
 @app.route("/cars/<car_id>",methods=['GET'])
-@crossdomain(origin='*')
 def GET_specific_cars(car_id):
 	for i in range(len(cars)):
                 if (str(cars[i]['id']) == str(car_id)):
@@ -17,14 +16,12 @@ def GET_specific_cars(car_id):
 
 
 @app.route("/cars",methods=['GET'])
-@crossdomain(origin='*')
 def GET_cars():
 	return_obj = {}
 	return_obj['cars'] = cars
 	return str(json.dumps(return_obj))
 
-@app.route("/cars",methods=['POST'])
-@crossdomain(origin='*')
+@app.route("/cars",methods=['POST','OPTIONS'])
 def POST_cars():
 	args = json.loads(request.data)
 	newCar = {}
@@ -32,11 +29,12 @@ def POST_cars():
 	newCar['brand'] = args['brand']
 	newCar['plateNumber'] =  args['plateNumber']
 	newCar['id'] = len(cars)
+	return_obj = {}
+	return_obj['car'] = newCar
 	cars.append(newCar)
-	return str(json.dumps(newCar))
+	return str(json.dumps(return_obj))
 
 @app.route("/cars/<car_id>",methods=['DELETE'])
-@crossdomain(origin='*')
 def DELETE_cars(car_id):
 	for i in range(len(cars)):
 		if (str(cars[i]['id']) == str(car_id)):
@@ -46,7 +44,6 @@ def DELETE_cars(car_id):
 	
 
 @app.route("/cars/<car_id>",methods=['PUT'])
-@crossdomain(origin='*')
 def UPDATE_cars(car_id):
 	args = json.loads(request.data)
 	for i in range(len(cars)):
